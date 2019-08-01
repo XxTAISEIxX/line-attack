5b4df7dba5353d588d30f764b3bc7e872d69655f
Taisei@memochang <41264845+XxTAISEIxX@users.noreply.github.com>
2019-08-01 23:01:05 +0900

    update s.py
    エラーでなくしました。

--- s.py
+++ s.py
@@ -10,6 +10,7 @@ params = {"category": "square", "mid": mid)}
 
 c = 0
 def post():
+    global c
     try:    requests.post("https://w.line.me/openchatactivate/api/unset", params=params)
     except: pass
     try:    requests.post("https://w.line.me/openchatactivate/api/set", params=params)
